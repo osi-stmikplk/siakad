471a0cf929104732a527acee18f69381319c7244
banghaji <muhammad651@gmail.com>
2016-05-31 16:26:58 +0700

    view FRS

--- resources/views/mahasiswa/frs/krs.blade.php
+++ resources/views/mahasiswa/frs/krs.blade.php
@@ -1,8 +1,30 @@
 {{--
 Cetak KHS
 --}}
-<h1>Kartu Rencana Study</h1>
-<table>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>{{ config('kejar.title') }}</title>
+    <link rel="stylesheet" href="{{ asset('reportingstyle.css') }}">
+	<link href="/plugins/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<script src="/plugins/bootstrap/jQuery-2.2.0.min.js"></script>
+	<script src="/plugins/bootstrap/bootstrap.min.js"></script>
+	
+	<style>
+	table { font-size:13px; }
+	th { text-align: center; }
+	tr, td { border: solid 0px #fff; }
+	</style>
+</head>
+<body>
+<!-- Site wrapper -->
+<div class="page container">
+    <!-- Main content -->
+    <div class="col-sm-offset-2 col-sm-8 col-sm-offset-2">
+<h1 class="text-center">Formulir Rencana Studi</h1>
+<br>
+<table class="table table-condensed noborder">
     <tr>
         <td>NIM</td>
         <td>{{ $mhs->nomor_induk }}</td>
@@ -22,8 +44,9 @@ Cetak KHS
         <td>{{ $ta }}</td>
     </tr>
 </table>
+
 <?php $i = $jumlahSKS = 0; ?>
-<table border="1">
+<table class="table table-bordered table-striped table-condensed">
     <thead>
         <tr>
             <th>No</th>
@@ -36,10 +59,10 @@ Cetak KHS
     </thead>
     <tbody>
     @foreach($rincianStudi as $ris)
-        <tr>
+        <tr align="center">
             <td>{{ ++$i }}</td>
             <td>{{ $ris->kode_mk }}</td>
-            <td>{{ $ris->nama_mk }}</td>
+            <td align="left">{{ $ris->nama_mk }}</td>
             <td>{{ $ris->sks }}</td>
             <td>{{ $ris->kelas }}</td>
             <td>&nbsp;</td>
@@ -49,17 +72,24 @@ Cetak KHS
     </tbody>
     <tfoot>
         <tr>
-            <td colspan="3">Jumlah SKS</td>
-            <td>{{ number_format($jumlahSKS) }}</td>
-            <td colspan="2">&nbsp;</td>
+            <th colspan="3">Jumlah SKS</th>
+            <th>{{ number_format($jumlahSKS) }}</th>
+            <th colspan="2">&nbsp;</th>
         </tr>
     </tfoot>
 </table>
-<p>
+<p class="pull-right">
     Palangka Raya,<br>
     Pembimbing Akademik<br>
 <br>
 <br>
     ______________________________
+	<br><br>
+
+	Tanggal Cetak {{ date('d-m-Y H:i') }}
 </p>
-<p>Tanggal Cetak {{ date('d-m-Y H:i') }}</p>
+    </div>
+</div>
+</body>
+</html>
+
