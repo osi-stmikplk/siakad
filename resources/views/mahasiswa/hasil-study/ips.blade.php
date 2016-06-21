1497ae8cc213197a093a25f9ef700aea3982459f
banghaji <muhammad651@gmail.com>
2016-06-21 17:12:40 +0700

    tambah tombol cetak KHS di modal riwayat akademik

--- resources/views/mahasiswa/hasil-study/ips.blade.php
+++ resources/views/mahasiswa/hasil-study/ips.blade.php
@@ -3,30 +3,33 @@ Load berdasarkan data yang dimasukkan adalah nilai IPS yang dibagi per semester.
 mengikutsertakan layout utama, artinya agar yang memanggil ini melakukan setting nya, hal ini mengingat view ini akan
 di load menggunakan format html dan juga bisa dijadikan pdf saat ingin di cetak
 --}}
-<table class="table">
+
+<style type="text/css">
+th { text-align: center; }
+</style>
+
+<table class="table table-bordered table-condensed table-striped table-hover">
     <thead>
     <tr>
         <th>Semester</th>
         <th>SKS * Nilai Bobot</th>
         <th>Jumlah SKS</th>
         <th>IPS</th>
+        <th>Cetak KHS</th>
     </tr>
     </thead>
     <tbody>
     @foreach($data as $d)
-        <tr>
+        <tr align="center">
+            <td>{{ $d->semester }}</td>
+            <td>{{ $d->jumBobotSKS }}</td>
+            <td>{{ $d->jumSKS }}</td>
+            <td>{{ number_format($d->jumBobotSKS / $d->jumSKS, 2, ",", ".") }}</td>
             <td>
-                {{ $d->semester }}
-            </td>
-            <td>
-                {{ $d->jumBobotSKS }}
-            </td>
-            <td>
-                {{ $d->jumSKS }}
-            </td>
-            <td>
-                {{ number_format($d->jumBobotSKS / $d->jumSKS, 2, ",", ".") }}
-            </td>
+				<button class="btn btn-xs btn-info" title="belum dibikin kata banghaji :)">
+					<span class="glyphicon glyphicon-print"></span>
+				</button>
+			</td>
         </tr>
     @endforeach
     </tbody>
