fc9b93bc4214eb16a33b9646b9247c4be88d48eb
yfktn <friskantoni@gmail.com>
2016-06-24 08:51:05 +0700

    Tampilkan tombol cetak KRS untuk yang FRS telah disetujui #18

--- resources/views/mahasiswa/frs/index-frs-sudah-final.blade.php
+++ resources/views/mahasiswa/frs/index-frs-sudah-final.blade.php
@@ -12,6 +12,7 @@
                     <h2><i class="fa fa-fire"></i> Status FRS Telah Disetujui dan Bersifat Final</h2>
                     <p>Bila terhadap FRS yang telah disetujui terdapat perubahan, maka diharapkan menghubungi AKMA/Dosen
                         Wali untuk melakukan reset terhadap status pengisian.</p>
+                    <p><a target="_blank" href="{{ route('mhs.frs.cetakKRS') }}" class="btn btn-primary"><i class="fa fa-print"></i> Klik Untuk Mencetak KRS</a></p>
                 </div>
             </div>
         </div>
