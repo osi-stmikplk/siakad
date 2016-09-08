12e967260fdd89b9d6b7a46eb356bee27e50f2e9
banghaji dot com <muhammad651@gmail.com>
2016-09-08 09:25:47 +0700

    revisi form cetak absensi (#51)
    revisi form cetak absensi request by bidadari akma :sunglasses:
    
    - hilangkan border tabel header
    - buat border solid untuk tabel absensi
    - tambah baris kosong di bawah
    - buat kolom pertemuan ttd sesuai sks
    - tambah ttd dosen pengampu

--- resources/views/akma/dosen-kelas-mk/form-absensi.blade.php
+++ resources/views/akma/dosen-kelas-mk/form-absensi.blade.php
@@ -4,80 +4,112 @@
 <style type="text/css">
 .ttd {
     width: 50px;}
-.spacep {padding: 10px 0 10px 0; height: 40px;}
 @media print {
     thead{ display: table-header-group; }
+    }
+}
+.table td, .table th {
+    border-color: #000000 !important;
+	border: 1px;
+}
+.table.no-border tr td, .table.no-border tr th {
+  border-width: 0;
+}
+.table-bordered td, .table-bordered th, .table-bordered thead {
+    border-color: #000000 !important;
+    border: 1px solid #000000;
+}
+.kada-begaris tr, .kada-begaris td, .kada-begaris table {
+    border: 1px solid #ffffff;
 }
 </style>
-<h1 class="text-center">Daftar Hadir Mata Kuliah<br>
-    Kelas {{ $dataKelas->kelas }}</h1>
-<table class="table table-condensed">
+
+<?php
+if($dataKelas->mataKuliah->sks >= 4)
+    $kolom='24';
+else
+    $kolom='16'
+?>
+
+<h2 class="text-center">Daftar Hadir Mata Kuliah</h2>
+<table class="table no-border table-condensed kada-begaris">
     <tr>
-        <td>Mata Kuliah:</td>
-        <td>{{ $dataKelas->mataKuliah->nama }}</td>
-        <td>SKS:</td>
-        <td>{{ $dataKelas->mataKuliah->sks }}</td>
+        <td width = 10%>Mata Kuliah</td>
+        <td width = 1%> :</td>
+        <td width = 30%>{{ $dataKelas->mataKuliah->nama }}</td>
+        <td width = 12%>SKS</td>
+        <td width = 1%> :</td>
+        <td width = 15%>{{ $dataKelas->mataKuliah->sks }}</td>
     </tr>
     <tr>
-        <td>Dosen Pengampu:</td>
+        <td>Dosen Pengampu</td>
+        <td> :</td>
         <td>{{ $dataKelas->dosen->nama }}</td>
-        <td>Semester:</td>
-        <td>{{ $dataKelas->mataKuliah->semester }}</td>
+        <td>Kelas</td>
+        <td> :</td>
+        <td>{{ $dataKelas->kelas }}</td>
     </tr>
     <tr>
-        <td>Jurusan:</td>
+        <td>Jurusan</td>
+        <td> :</td>
         <td>{{ $dataKelas->mataKuliah->jurusan->nama }}</td>
-        <td>Tahun Ajaran:</td>
-        <td>{{ $dataKelas->tahun_ajaran }}</td>
+        <td>Semester / Tahun Ajaran</td>
+        <td> :</td>
+        <td>{{ $dataKelas->mataKuliah->semester }} / {{ $dataKelas->tahun_ajaran }}</td>
     </tr>
 </table>
 <table class="table table-bordered table-condensed">
     <thead>
     <tr>
-        <th rowspan="2"><nobr>No.</nobr></th>
-        <th rowspan="2">NIM</th>
-        <th rowspan="2">Nama</th>
-        <th colspan="16" style="text-align: center">Tanggal</th>
+        <th rowspan="3"><nobr><div align="center">No.</div></nobr></th>
+        <th rowspan="3"><div align="center">NIM</div></th>
+        <th rowspan="3"><div align="center">Nama</div></div></th>
+        <th colspan="{{ $kolom }}"><div align="center">Pertemuan / Tanggal</div></th>
     </tr>
     <tr>
-        @for($j=1;$j<=16;$j++)
-            <td>&nbsp;<br>&nbsp;</td>
+        @for($j=1;$j<=$kolom;$j++)
+            <td align="center">{{ $j }}</td>
+        @endfor
+    </tr>
+    <tr>
+        @for($j=1;$j<=$kolom;$j++)
+            <td>&nbsp;</td>
         @endfor
     </tr>
     </thead>
     <tbody>
     <?php $i=0 ?>
-    {{-- cetakan nama dan baris per mahasiswa --}}
     @foreach($mahasiswa as $m)
         <tr>
-            <td class="spacep"><?php echo ++$i; ?></td>
-            <td class="spacep">{{ $m->nomor_induk }}</td>
-            <td class="spacep"><nobr>{{ $m->nama }}</nobr></td>
-            @for($j=1;$j<=16;$j++)
-                <td class="spacep ttd">&nbsp;</td>
+            <td align="center"><?php echo ++$i; ?></td>
+            <td>{{ $m->nomor_induk }}</td>
+            <td><nobr>{{ $m->nama }}</nobr></td>
+            @for($j=1;$j<=$kolom;$j++)
+                <td class="ttd">&nbsp;</td>
             @endfor
         </tr>
     @endforeach
-    <?php $max = $i + 5; ?>
-    @while($i < $max)
+        @for($z=1;$z<=5;$z++)
         <tr>
-            <td class="spacep">&nbsp;</td>
-            <td class="spacep">&nbsp;</td>
-            <td class="spacep"><nobr>&nbsp;</nobr></td>
-            @for($j=1;$j<=16;$j++)
-                <td class="spacep ttd">&nbsp;</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            @for($x=1;$x<=$kolom;$x++)
+                <td class="ttd">&nbsp;</td>
             @endfor
         </tr>
-    @endwhile
+        @endfor
     </tbody>
 </table>
 
-<p class="pull-right">
-    Palangka Raya,<br>
-    Dosen Pengampu,<br>
-    <br>
-    <br>
-    _____________________________
-
+<p>
+    <div class="pull-right">
+        Palangka Raya, ____/____/________
+        <br>
+        <br>
+        <br>
+        <br>
+        <u>{{ $dataKelas->dosen->nama }}</u>
+    </div>
 </p>
 @endsection
